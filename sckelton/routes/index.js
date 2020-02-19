5f59173a0cac0dcdc642830f2006cb4f613657eb
skvrgit <skvrgit0317@gmail.com>
2020-02-19 23:03:18 +0900

    added

--- sckelton/routes/index.js
+++ sckelton/routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  const orderclass = class {
+  constructor(timestamp, instrument, buysell, lot, customer, sales) {
+	this.Timestamp = timestamp;
+	this.Instrument = instrument;
+	this.BuySell = buysell;
+	this.Lot = lot;
+	this.Customer = customer;
+	this.Sales = sales;
+  } }
+  
+  var result = new Array();
+  result.push(new orderclass("a","b","c","d","e","f"))
+  result.push(new orderclass("a","b","c","d","e","f"))
+
+  res.render('index', { title: 'Express' , orders:result});
 });
 
 module.exports = router;
